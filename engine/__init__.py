d59c8c5013bdf20e428fd8940941d040af495cb2
Hsieh, Chiao <hc825b@gmail.com>
2014-10-29 19:26:03 +0800

    Add directory structure for external tools.

--- engine/__init__.py
+++ engine/__init__.py
@@ -14,17 +14,17 @@ def _abs_path(rel_path):
   global _this_dir_g
   return os.path.join(_this_dir_g, rel_path)
 
-transform_manager.set_tran_path(_abs_path("../cil/tran.native"))
-transform_manager.set_output_path(_abs_path("../tmp/"))
+transform_manager.set_tran_path(_abs_path("../tran/tran.native"))
+transform_manager.set_output_path(_abs_path("../var/"))
 
 rl_mgr.setExePath(_abs_path("../ext_quantifier/reduce/reduce"))
-rl_mgr.setScriptPath(_abs_path("../tmp/rl_script"))
-rl_mgr.setOutputPath(_abs_path("../tmp/rl_summary"))
+rl_mgr.setScriptPath(_abs_path("../var/rl_script"))
+rl_mgr.setOutputPath(_abs_path("../var/rl_summary"))
 
 options = {
   "bin_file"  :_abs_path("../ext_analyzer/CPAchecker-1.3.4-unix/scripts/cpa.sh") ,
   "conf_file" :_abs_path("../ext_analyzer/CPAchecker-conf/UsedConfiguration.properties"),
-  "output_dir":_abs_path("../tmp/proof")}
+  "output_dir":_abs_path("../var/proof")}
 basic_analyzer.set_paths(options)
 
 # Register available analyzers
