db768f546b18ad2c13655199e5e145131c83b1ef
Hsieh, Chiao <hc825b@gmail.com>
2014-10-30 11:36:08 +0800

    Create temporary directory to avoid read/write on same file
    Less print lines

--- engine/__init__.py
+++ engine/__init__.py
@@ -1,4 +1,6 @@
 import os
+import tempfile
+import shutil
 
 from transform_manager import Program
 import overview
@@ -14,17 +16,20 @@ def _abs_path(rel_path):
   global _this_dir_g
   return os.path.join(_this_dir_g, rel_path)
 
+_tmp_dir_g = tempfile.mkdtemp(dir = _abs_path("../var/"))
+
+# TODO better contorl of configuration
 transform_manager.set_tran_path(_abs_path("../tran/tran.native"))
-transform_manager.set_output_path(_abs_path("../var/"))
+transform_manager.set_output_path(_tmp_dir_g)
 
 rl_mgr.setExePath(_abs_path("../ext_quantifier/reduce/reduce"))
-rl_mgr.setScriptPath(_abs_path("../var/rl_script"))
-rl_mgr.setOutputPath(_abs_path("../var/rl_summary"))
+rl_mgr.setScriptPath(_abs_path(_tmp_dir_g + "/rl_script"))
+rl_mgr.setOutputPath(_abs_path(_tmp_dir_g + "/rl_summary"))
 
 options = {
   "bin_file"  :_abs_path("../ext_analyzer/CPAchecker-1.3.4-unix/scripts/cpa.sh"),
   "conf_file" :_abs_path("../ext_analyzer/CPAchecker-conf/UsedConfiguration.properties"),
-  "output_dir":_abs_path("../var/proof")}
+  "output_dir":_abs_path(_tmp_dir_g + "/proof")}
 basic_analyzer.set_paths(options)
 
 # Register available analyzers
@@ -34,6 +39,7 @@ def run(design_file):
   try:
     result = overview.run( Program(design_file) )
     if result == 'Error':
+      # TODO Move error report to Error class
       print ((" Error Witness ").center(70, '='))
       print ("Values changes of variables in main() and global variables")
       assignment = result.get_variable_assignment()
@@ -42,5 +48,7 @@ def run(design_file):
           print (var + ': ' + str(values))
   except RuntimeError:
     result = 'Unknown'
+
+  shutil.rmtree(_tmp_dir_g, ignore_errors=True)
 
   return result
--- engine/overview.py
+++ engine/overview.py
@@ -1,3 +1,6 @@
+import sys
+from six import print_
+
 import basic_analyzer
 
 from approx import unwind
@@ -25,24 +28,27 @@ def overview(program):
   while not complete:
     k = k + 1
 
-    print ("Current unwinding depth: " + str(k))
+    print_progress ("Unwinding: " + str(k))
     P_k = unwind(program, k)
 
     G_main = under_approx(P_k)
-    print ("Analyzing under-approximation:")
+    print_progress (" -> Analyzing under-approximation: ")
     result = basic_analyzer.analyze(G_main)
     print (result)
 
     if result == 'Error' or result == 'Unknown':
       return result
     # else: # result == Pass
-    print ("Computing Candidate of Summaries")
+    print_progress (" -> Computing Summaries")
     S = compute_summary(P_k, result)
 
-    print ("Checking Summaries:")
+    print_progress (" -> Checking Summaries: ")
     result = check_summary(P_k, S)
     print (result)
     complete = (result == 'Pass')
 
   return result
 
+def print_progress(string):
+  print_ (string, end='')
+  sys.stdout.flush()
